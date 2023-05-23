ed5c74e9f667e9c8273bd4059355fc65c6678fae
liu_bili_Grass <128198000+liubili@users.noreply.github.com>
2023-05-24 02:58:12 +0800

    Add files via upload

--- ffmepg/ffmepg.py
+++ ffmepg/ffmepg.py
@@ -11,7 +11,7 @@ layout = [
     [sg.Text("请选择输出文件夹：")],
     [sg.Input(key="-FOLDER-"), sg.FolderBrowse()],
     [sg.Button("查询支持解码的环境")],
-    [sg.Combo(["默认解码", "硬解", "mkv", "webm"], key="-打开文件-")]
+    [sg.Combo(["", "-c:v h264_cuvid", "-hwaccel cuda", "-hwaccel_device 0 -hwaccel cuda"], key="解码")],
 ]
 
 # 创建窗口
@@ -21,6 +21,7 @@ window = sg.Window("使用PySimpleGUI实现的视频转换器", layout)
 while True:
     event, values = window.read()
     # 根据事件和值进行逻辑处理
+    jiema = values["解码"]
     if event == "开始转换":
         # 获取用户选择的文件名和格式
         filename = values["-FILE-"]
@@ -28,9 +29,11 @@ while True:
         # 检查文件名和格式是否有效
         if filename and format:
             # 生成输出文件名
-            output = values["-FOLDER-"] + "/" + filename.rsplit(".", 1)[0] + "." + format
+            output = (
+                values["-FOLDER-"] + "/" + filename.rsplit(".", 1)[0] + "." + format
+            )
             # 调用ffmpeg进行视频转换
-            cmd = ["ffmpeg", "-i", filename, output]
+            cmd = ["ffmpeg", jiema,"-i", filename, output]
             subprocess.run(cmd)
             # 弹出对话框提示转换完成
             sg.popup(f"视频转换完成，输出文件为{output}")
@@ -38,8 +41,10 @@ while True:
             # 弹出对话框提示输入错误
             sg.popup("请输入有效的文件名和格式。")
     elif event == "查询支持解码的环境":
-        cmdload = ["ffmpeg", "version"]
+        cmdload = ["ffmpeg", "-hwaccel"]
+        cmdload = ["ffmpeg", "-version"]
         subprocess.run(cmdload)
+        continue
     elif event == "退出" or event == sg.WIN_CLOSED:
         # 退出循环
         break
