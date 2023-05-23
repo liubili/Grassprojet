1c70f63fb1a9673fca764325b586c78389256166
liu_bili_Grass <128198000+liubili@users.noreply.github.com>
2023-05-24 05:41:20 +0800

    Add files via upload

---
+++ ffmepg/启动.py
@@ -0,0 +1,6 @@
+import os
+os.system('pip install pyinstaller')
+os.system('pip install cloc')
+os.system('pip install pysimplegui')
+os.system('python.exe -m pip install --upgrade pip')
+os.system("python ./ffmepg.py")
